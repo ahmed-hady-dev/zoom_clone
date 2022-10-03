8d2552f3989d6aa4deaaa1d76d15e6329a7da2e0
ahmed <ahmed.hady.dev@gmail.com>
2022-10-04 01:28:21 +0200

    #03 firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'core/getStorageCacheHelper/get_storage_cache_helper.dart';
+import 'firebase_options.dart';
 import 'view/home/home_view.dart';
 import 'core/theme/theme.dart';
 import 'core/theme/theme_cubit.dart';
@@ -15,7 +16,7 @@ import 'core/router/router.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   //===============================================================
-  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   // Widget? home;
   // FirebaseAuth.instance.userChanges().listen((user) {
   //   if (user == null) {
