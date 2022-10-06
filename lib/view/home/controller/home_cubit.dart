6deec675e596217d44021cd38ea8255a32a995ac
ahmed <ahmed.hady.dev@gmail.com>
2022-10-06 14:56:42 +0200

    #08 new meeting functionality added

--- lib/view/home/controller/home_cubit.dart
+++ lib/view/home/controller/home_cubit.dart
@@ -1,6 +1,9 @@
 import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:jitsi_meet/feature_flag/feature_flag.dart';
+import 'package:jitsi_meet/jitsi_meet.dart';
 import 'package:meta/meta.dart';
 import 'package:zoom_clone/view/home/history_meeting_view.dart';
 
@@ -14,7 +17,7 @@ class HomeCubit extends Cubit<HomeState> {
   static HomeCubit get(context) => BlocProvider.of(context);
   //===============================================================
   int _currentIndex = 0;
-
+  final firebaseUser = FirebaseAuth.instance.currentUser!;
   int get currentIndex => _currentIndex;
   final List<Widget> _homePages = const [
     MeetingView(),
@@ -28,5 +31,28 @@ class HomeCubit extends Cubit<HomeState> {
   void changePage(int index) {
     _currentIndex = index;
     emit(ChangeBottomBavBarIndex());
+  }
+
+  createMeeting({
+    required String roomName,
+    required bool isAudioMuted,
+    required bool isVideoMuted,
+  }) async {
+    try {
+      FeatureFlag featureFlag = FeatureFlag();
+      featureFlag.welcomePageEnabled = false;
+      featureFlag.resolution = FeatureFlagVideoResolution.MD_RESOLUTION; // Limit video resolution to 360p
+
+      var options = JitsiMeetingOptions(room: roomName)
+        ..userDisplayName = firebaseUser.displayName
+        ..userEmail = firebaseUser.email
+        ..userAvatarURL = firebaseUser.photoURL
+        ..audioMuted = isAudioMuted
+        ..videoMuted = isVideoMuted;
+
+      await JitsiMeet.joinMeeting(options);
+    } catch (error) {
+      debugPrint("error: $error");
+    }
   }
 }
--- lib/view/home/meeting_view.dart
+++ lib/view/home/meeting_view.dart
@@ -1,5 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:zoom_clone/components/home_meeting_button.dart';
+
+import 'controller/home_cubit.dart';
 
 class MeetingView extends StatelessWidget {
   const MeetingView({
@@ -8,22 +13,33 @@ class MeetingView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            HomeMeetingButton(onPressed: () {}, icon: Icons.videocam, text: 'New Meeting'),
-            HomeMeetingButton(onPressed: () {}, icon: Icons.add_box_rounded, text: 'Join Meeting'),
-            HomeMeetingButton(onPressed: () {}, icon: Icons.calendar_today, text: 'Schedule'),
-            HomeMeetingButton(onPressed: () {}, icon: Icons.arrow_upward_rounded, text: 'Share Screen'),
-          ],
-        ),
-        Expanded(
-            child: Center(
-                child: Text('Create/Join Meetings with just a click',
-                    style: Theme.of(context).textTheme.titleMedium!.copyWith(fontWeight: FontWeight.bold))))
-      ],
+    final cubit = HomeCubit.get(context);
+    var ramdom = Random();
+    String roomName = (ramdom.nextInt(1000000) + 1000000).toString();
+    return BlocListener<HomeCubit, HomeState>(
+      listener: (context, state) {},
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              HomeMeetingButton(
+                  onPressed: () {
+                    cubit.createMeeting(roomName: roomName, isAudioMuted: true, isVideoMuted: true);
+                  },
+                  icon: Icons.videocam,
+                  text: 'New Meeting'),
+              HomeMeetingButton(onPressed: () {}, icon: Icons.add_box_rounded, text: 'Join Meeting'),
+              HomeMeetingButton(onPressed: () {}, icon: Icons.calendar_today, text: 'Schedule'),
+              HomeMeetingButton(onPressed: () {}, icon: Icons.arrow_upward_rounded, text: 'Share Screen'),
+            ],
+          ),
+          Expanded(
+              child: Center(
+                  child: Text('Create/Join Meetings with just a click',
+                      style: Theme.of(context).textTheme.titleMedium!.copyWith(fontWeight: FontWeight.bold))))
+        ],
+      ),
     );
   }
 }
